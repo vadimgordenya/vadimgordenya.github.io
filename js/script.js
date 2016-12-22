d182deebd456a4aac81c580e9e26af5b75e12e5a
Вадим Горденя <vadimgordenya@mail.ru>
2016-12-22 21:40:27 +0300

    respons portfolio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+ $(document).ready(function(){
+   var url=document.location.href;
+          $.each($(".menu__link li a"),function(){
+    if(this.href==url){$(this).addClass('link__active').css("color","#fff");};
+   });
+    $(".menu-trigger").click(function() {
+		$(".menu ul").slideToggle(500);
+	});
+	 $(window).resize(function() {		
+		if ( $(window).width() > 680 ) {			
+			$('.menu ul').removeAttr('style');
+		 }
+	});      
+});
+
+
