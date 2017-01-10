21344d71c748ddc7540226da18c49d366995cd2f
Vadim <vadimgordenya@gmail.com>
2017-01-10 16:35:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(window).on('load', function() {
         $(".user__info").typed({
             strings: ["<div><span class='bold'>Меня зовут :</span> Горденя Вадим Викторович</div><div><span class='bold'>Мой возраст :</span> 21 год</div><div><span class='bold'>Мой город :</span> Гродно, Беларусь</div><div><span class='bold'>Моя специальность :</span> FRONTEND разработчик</div><div><span class='bold key__skills'>Ключевые навыки :</span><div class='wrapp__skills'><span class='skills'>html</span><span class='skills'>CSS</span><span class='skills'>javascript</span><br><span class='skills'>git</span><span class='skills'>gulp</span></div> "],
             typeSpeed: 30,
-            startDelay: 2000
+            startDelay: 2500
         });
     });
 }); 
