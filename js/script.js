abb4bd5c493a67b0085242fb04dc3c5764e6661b
Vadim <vadimgordenya@gmail.com>
2017-01-15 13:52:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(window).on('load', function() {
 	
     $(function() {
         $(".user__info").typed({
-            strings: ["<div><span class='bold'>Меня зовут :</span> Горденя Вадим Викторович</div><div><span class='bold'>Мой возраст :</span> 21 год</div><div><span class='bold'>Мой город :</span> Гродно, Беларусь</div><div><span class='bold'>Моя специальность :</span> FRONTEND разработчик</div><div><span class='bold key__skills'>Ключевые навыки :</span><div class='wrapp__skills'><span class='skills'>html</span><span class='skills'>CSS</span><span class='skills'>Bootstrap</span><span class='skills'>Sass</span><span class='skills'>javascript</span><br><span class='skills'>git</span><span class='skills'>gulp</span></div> "],
+            strings: ["<div><span class='bold'>Меня зовут :</span> Горденя Вадим Викторович</div><div><span class='bold'>Мой возраст :</span> 21 год</div><div><span class='bold'>Мой город :</span> Гродно, Беларусь</div><div><span class='bold'>Моя специальность :</span> FRONTEND разработчик</div><div><span class='bold key__skills'>Ключевые навыки :</span><div class='wrapp__skills'><span class='skills'>HTML5</span><span class='skills'>CSS3</span><span class='skills'>Bootstrap</span><span class='skills'>Sass</span><span class='skills'>javascript</span><br><span class='skills'>git</span><span class='skills'>gulp</span></div> "],
             typeSpeed: 30,
             startDelay: 2500
         });
